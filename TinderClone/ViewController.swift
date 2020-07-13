cfe31419beac7a4dca6c39b25063ce9e4cc1d8e9
Rizaldidev <rizaldi@stockbit.com>
2020-07-13 20:15:09 +0700

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Rizaldi Nur Muhammad on 13/07/20.
+//  Copyright © 2020 Rizaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
